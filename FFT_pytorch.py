91c8f782e183f86b39527da4bd2bb799fcd318f4
Matteo Stefanini <MatteoStefanini@users.noreply.github.com>
2018-06-25 21:20:00 +0200

    Update FFT_pytorch.py

--- FFT_pytorch.py
+++ FFT_pytorch.py
@@ -1,35 +1,10 @@
 import numpy as np
 import torch
 from torch.autograd import Variable
-def DFT_slow(x):
-    """Compute the discrete Fourier Transform of the 1D array x"""
-    x = np.asarray(x, dtype=float)
-    N = x.shape[0]
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    M = np.exp(-2j * np.pi * k * n / N)
-    return np.dot(M, x)
-
-
-def FFT(x):
-    """A recursive implementation of the 1D Cooley-Tukey FFT"""
-    x = np.asarray(x, dtype=float)
-    N = x.shape[0]
-
-    if N % 2 > 0:
-        raise ValueError("size of x must be a power of 2")
-    elif N <= 32:  # this cutoff should be optimized
-        return DFT_slow(x)
-    else:
-        X_even = FFT(x[::2])
-        X_odd = FFT(x[1::2])
-        factor = np.exp(-2j * np.pi * np.arange(N) / N)
-        return np.concatenate([X_even + factor[:N / 2] * X_odd,
-                               X_even + factor[N / 2:] * X_odd])
 
 
 def FFT_vectorized(x):
-    """A vectorized, non-recursive version of the Cooley-Tukey FFT"""
+    """A vectorized, non-recursive version of the Cooley-Tukey FFT with Numpy"""
     x = np.asarray(x, dtype=float)
     N = x.shape[0]
 
@@ -56,9 +31,9 @@ def FFT_vectorized(x):
 
 
 def FFT_torch_simple_signal(x):
-    # Enable next 2 lines for numpy comparison (if you come in with a numpy tensor) otherwise MUST be commented
-    x = np.asarray(x, dtype=float)
-    x = Variable(torch.from_numpy(x), requires_grad=True).double().cuda()
+    # Enable next 2 lines for if you come in with a numpy tensor otherwise MUST be commented
+    #x = np.asarray(x, dtype=float)
+    #x = Variable(torch.from_numpy(x), requires_grad=True).double().cuda()
     N_samples = x.shape[0]
 
     if np.log2(N_samples) % 1 > 0:
